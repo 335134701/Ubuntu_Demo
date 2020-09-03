4aefd929bfcb973d7c035bbe50638999e72f3077
zc <33513701@qq.com>
2020-09-03 21:43:50 +0800

    2020-09-03 21:43:50 : 修改GIt脚本

--- Git_Pull.sh
+++ Git_Pull.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+
 #判断命令是否执行成功
 #${1}:执行的命令语句
 function Judge_Order(){
@@ -7,9 +8,9 @@ function Judge_Order(){
 	if [ ${#} -eq 3 ];then
 		#判断上一条命令的返回值是否为0，若为0则执行成功，若不为0则执行失败
 		if [ ${status} -eq 0 ];then
-			echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m\"${1}\" \033[0m"
+			echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m${1}\033[0m"
 		else 
-			echo -e "[\033[33m$(date +"%Y-%m-%d %T") Warning\033[0m]  ""\033[33m\"${2}\" \033[0m"
+			echo -e "[\033[33m$(date +"%Y-%m-%d %T") Warning\033[0m]  ""\033[33m${2}\033[0m"
 			if [ "${3}" -eq 0 ];then
 				exit 127
 			fi
@@ -19,24 +20,57 @@ function Judge_Order(){
 		return 80
 	fi
 }
-
-function Main(){
+function Main()
+{
 	#=======================================================
 	#参数更改
 	repository=git@github.com:335134701/Linux_System_Setting.git
-	#=======================================================
 	localDir=$(pwd)
 	localDirName=${localDir##*/}
+	#=======================================================
 	git --version >/dev/null 2>&1
 	Judge_Order "Git已安装!" "Git未安装!" 0
-	git checkout -b ${localDirName}  >/dev/null 2>&1
-	if [ ${?} -eq 128 ];then
+	if [ ! -d ${localDir}/.git ];then
+		git init >/dev/null 2>&1
+		Judge_Order "git init成功!" "git init失败!" 0
+	fi
+	#添加远程仓库 git remote add <名称> <URL>
+	git remote add origin ${repository}
+	Judge_Order "添加远程仓库成功!" "添加远程仓库失败,远程仓库已存在!" 1
+	#解决Permission denied的问题
+	eval "$(ssh-agent -s)" ssh-add
+	#判断本地有无${localDirName}分支,若没有则创建本地${localDirName}分支,并创建提交文件README.md
+	$(git branch |grep -q "${localDirName}")
+	if [ ${?} -eq 0 ];then
 		git checkout ${localDirName}  >/dev/null 2>&1
 		Judge_Order "切换至${localDirName}分支成功!" "切换至${localDirName}分支失败!" 0
+	elif [ ${?} -eq 1 ];then
+		git checkout -b ${localDirName}  >/dev/null 2>&1
+		Judge_Order "创建${localDirName}分支成功!" "创建${localDirName}分支失败!" 0
 	fi
-	eval "$(ssh-agent -s)" 
-	ssh-add
-	git pull origin ${localDirName}
-	Judge_Order "获取最新${localDirName}分支成功!" "获取最新${localDirName}分支失败!" 0
+	#判断本地分支与远程分支是否关联,如果没有则创建或建立连接(强调本地分支与远程分支同名)
+	$(git branch -a |grep -q "remotes/origin/${localDirName}")
+	if [ ${?} -ne 0 ];then
+		#拉取远程分支到本地分支,如果拉取不成功则强制推送本地分支到远程并创建同名分支
+		git fetch origin ${localDirName}
+		if [ ${?} -eq 0 ];then
+			echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m拉取远程${localDirName}分支成功!\033[0m"
+			git pull origin ${localDirName} >/dev/null 2>&1
+			Judge_Order "获取最新的${localDirName}分支成功" "获取最新的${localDirName}分支失败!" 0
+		else
+			echo "# ${localDirName}分支" >> README.md
+			git add README.md >/dev/null 2>&1
+			git commit -m "${localDirName}分支创建并提交!" >/dev/null 2>&1
+			Judge_Order "本地提交README.md成功!" "本地提交README.md失败!" 0 
+			#如果远程不存在与本地分支同名的分支则强制推送本地分支到远程并创建同名分支
+			git push -u origin ${localDirName} >/dev/null 2>&1
+			Judge_Order "创建并推送远程${localDirName}分支成功!" "创建并推送远程${localDirName}分支失败!" 0 
+		fi
+	else
+		git pull origin ${localDirName} >/dev/null 2>&1
+		Judge_Order "获取最新的${localDirName}分支成功" "获取最新的${localDirName}分支失败!" 0
+	fi
+
 }
+
 Main
--- Git_Push.sh
+++ Git_Push.sh
@@ -7,9 +7,9 @@ function Judge_Order(){
 	if [ ${#} -eq 3 ];then
 		#判断上一条命令的返回值是否为0，若为0则执行成功，若不为0则执行失败
 		if [ ${status} -eq 0 ];then
-			echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m\"${1}\" \033[0m"
+			echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m${1}\033[0m"
 		else 
-			echo -e "[\033[33m$(date +"%Y-%m-%d %T") Warning\033[0m]  ""\033[33m\"${2}\" \033[0m"
+			echo -e "[\033[33m$(date +"%Y-%m-%d %T") Warning\033[0m]  ""\033[33m${2}\033[0m"
 			if [ "${3}" -eq 0 ];then
 				exit 127
 			fi
@@ -24,34 +24,35 @@ function Remote_Push()
 {
 	echo
 	#添加远程仓库 git remote add <名称> <URL>
-	git remote add origin ${repository}
-	Judge_Order "添加远程仓库成功!" "添加远程仓库失败!" 1
-	git push -u origin ${localDirName}
-	Judge_Order "推送到远程仓库成功" "推送到远程仓库失败" 0
+	git remote add origin ${repository} >/dev/null 2>&1
+	Judge_Order "添加远程仓库成功!" "添加远程仓库失败,远程仓库已存在!" 1
+	git push -u origin ${localDirName} >/dev/null 2>&1
+	Judge_Order "推送到远程${localDirName}分支成功!" "推送到远程${localDirName}分支失败!" 0
 	echo
 }
 #本地提交
 function Local_Push()
 {
 	echo 
-	read -t 20 -p "Input a text to describe the change:" input
+	read -t 60 -p "Input a text to describe the change:" input
 	if [ -z ${input} ];then
 		echo
 		echo -e "[\033[31m$(date +"%Y-%m-%d %T") Error\033[0m] Your input is timeout!"
 		exit 127
 	fi
 	#将文件的修改、文件的删除、文件的新建，添加到暂存区
-	git add -A
-	git commit -m "$(date "+%Y-%m-%d %H:%M:%S") : ${input}"	
+	git add -A >/dev/null 2>&1
+	git commit -m "$(date "+%Y-%m-%d %H:%M:%S") : ${input}"	>/dev/null 2>&1
+	Judge_Order "本地${localDirName}分支提交更新成功!" "本地${localDirName}分支提交更新失败!" 0
 	echo 
 }
 function Main(){
 	#=======================================================
 	#参数更改
-	repository=git@github.com:335134701/Ubuntu_Demo.git
-	#=======================================================
+	repository=git@github.com:335134701/Linux_System_Setting.git
 	localDir=$(pwd)
 	localDirName=${localDir##*/}
+	#=======================================================
 	git --version >/dev/null 2>&1
 	Judge_Order "Git已安装!" "Git未安装!" 0
 	git checkout -b ${localDirName}  >/dev/null 2>&1
@@ -59,12 +60,16 @@ function Main(){
 		git checkout ${localDirName}  >/dev/null 2>&1
 		Judge_Order "切换至${localDirName}分支成功!" "切换至${localDirName}分支失败!" 0
 	fi
-	eval "$(ssh-agent -s)" 
-	ssh-add
-	git pull origin ${localDirName}
+	eval "$(ssh-agent -s)" ssh-add
+	git pull origin ${localDirName} >/dev/null 2>&1
 	Judge_Order "获取最新${localDirName}分支成功!" "获取最新${localDirName}分支失败!" 0
-	Local_Push
-	Remote_Push
+	updateCount=(`git status -s`)
+	if [ ${#updateCount[*]} -ne 0 ];then
+		Local_Push
+		Remote_Push
+	else
+		echo -e "[\033[32m$(date +"%Y-%m-%d %T") Info\033[0m]  ""\033[32m暂无需要更新内容!\033[0m"
+	fi
 }
 
 Main
